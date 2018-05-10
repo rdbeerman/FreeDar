debcc5e6bbb560dc95e58ea3ffa7045a5282766a
R.D. Beerman <rdbeerman@users.noreply.github.com>
2018-05-10 09:48:30 +0200

    arduino firmware v2
    uses a prototype with servo and single vl53l0x

--- Arduino/VL52L0X/Continuous/Continuous.ino
+++ Arduino/VL52L0X/Continuous/Continuous.ino
@@ -6,14 +6,18 @@ The range readings are in units of mm. */
 
 #include <Wire.h>
 #include <VL53L0X.h>
+#include <Servo.h>
 
 VL53L0X sensor;
+Servo servo;
+
+int pos = 0;
 
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
-
+  servo.attach(9);
   sensor.init();
   sensor.setTimeout(500);
 
@@ -26,8 +30,20 @@ void setup()
 
 void loop()
 {
-  Serial.print(sensor.readRangeContinuousMillimeters()-45);
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-
-  Serial.println();
+  for (pos = 0; pos <= 180; pos += 5) {
+    servo.write(pos);
+    Serial.print(sensor.readRangeContinuousMillimeters()-45);
+    Serial.print(" ");
+    Serial.print(pos);
+    if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+    Serial.println();
+  }
+  for (pos = 180; pos >= 0; pos -= 5) {
+    servo.write(pos);
+    Serial.print(sensor.readRangeContinuousMillimeters()-45);
+    Serial.print(" ");
+    Serial.print(pos);
+    if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+    Serial.println();
+  }
 }
