a5829b1360a2cce45debfb2de09df33b36e1adeb
R.D. Beerman <rdbeerman@users.noreply.github.com>
2018-05-12 17:40:46 +0200

    added ultrasonic code
    initial results not promising; sensor is very angle dependent ie. only gets accurate measurement on perpendicular objects.

--- Arduino/Ultrasonic/Ultrasonic.ino
+++ Arduino/Ultrasonic/Ultrasonic.ino
@@ -3,30 +3,60 @@
  * Author:        R.D. Beerman
  * Date:          18/04/2018
 */
-const int trigPin = 9;
-const int echoPin = 10;
+#include <Servo.h>;
+#include <Wire.h>;
+Servo servo;
+
+#define echoPin 2 
+#define trigPin 3 
+
+int pos = 0;
 
 long duration;
 float distance;
 
 void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  servo.attach(9);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  Serial.begin(9600);
 }
 
 void loop() {
-  // clear trigpin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // start measurement
-  digitalWrite(trigPin,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // read result
-  duration = pulseIn(echoPin, HIGH);
-  // calc distance
-  distance = duration*0.00034/2;
-  //send over serial port
-  Serial.println(distance);
+  for (pos = 0; pos <= 180; pos += 5) {
+    servo.write(pos);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration*0.34/2;
+    Serial.print(distance);
+    Serial.print(" ");
+    Serial.print(pos*3.14159/180);
+    Serial.println();
+    //delay(50);      // calm down
+  }
+ for (pos = 180; pos >= 0; pos -= 5) {
+    servo.write(pos);
+    // clear trigpin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // start measurement
+    digitalWrite(trigPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // read result
+    duration = pulseIn(echoPin, HIGH);
+    // calc distance
+    distance = duration*0.34/2;
+    //send over serial port
+    Serial.print(distance);
+    Serial.print(" ");
+    Serial.print(pos*3.14159/180);
+    Serial.println();
+    //delay(50);    //calm down
+  }
 }
